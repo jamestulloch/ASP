8d344444957744288b4de4eb2c591ffbf4739cec
j2-tulloch <james2.tulloch@live.uwe.ac.uk>
2023-11-10 13:28:49 +0000

    Upload New File

---
+++ Final/Task4/my_string.hpp
@@ -0,0 +1,30 @@
+#ifndef MY_STRING_HPP
+#define MY_STRING_HPP
+
+#include "refcount.hpp"
+#include <iostream>
+#include <cstring>
+
+class my_string {
+private:
+    RefCount<char> data; // Use RefCount to manage the char array
+
+public:
+    // Constructors
+    my_string(); // Default constructor
+    my_string(const char* str); // Constructor from C-style string
+    my_string(const my_string& s); // Copy constructor
+
+    // Destructor
+    ~my_string();
+
+    // Operators
+    my_string& operator=(const my_string& s); // Copy assignment operator
+
+    // Utility functions
+    char getChar(const int& i) const; // Get a character at a given index
+    void setChar(const int& i, const char& c); // Set a character at a given index
+    void print() const; // Print the string with reference count
+};
+
+#endif // MY_STRING_HPP
