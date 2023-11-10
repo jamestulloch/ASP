3e094865f262a5bbdd61032a57237559afca11a7
j2-tulloch <james2.tulloch@live.uwe.ac.uk>
2023-11-10 13:26:03 +0000

    Upload New File

---
+++ Final/Task2/my_string.hpp
@@ -0,0 +1,32 @@
+// Ensure the header is only included once per compilation unit
+#ifndef MY_STRING_HPP
+#define MY_STRING_HPP
+
+#include <iostream>
+#include <cstring>
+
+// my_string class declaration
+class my_string {
+private:
+    char* data; // The data pointer for the string's characters
+    int* reference_count; // The pointer to this string's reference count
+
+public:
+    // Constructors
+    my_string(); // Default constructor
+    my_string(const char* str); // Parameterized constructor for C-string
+    my_string(const my_string& s); // Copy constructor
+
+    // Destructor
+    ~my_string();
+
+    // Operators
+    my_string& operator=(const my_string& s); // Copy assignment operator
+
+    // Utility functions
+    char getChar(const int& i) const; // Get a character at a given index
+    void setChar(const int& i, const char& c); // Set a character at a given index
+    void print() const; // Print the string
+};
+
+#endif // MY_STRING_HPP
