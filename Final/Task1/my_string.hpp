deaa484f08c0b23840b8f22b69479be28b338dd1
j2-tulloch <james2.tulloch@live.uwe.ac.uk>
2023-11-11 09:09:02 +0000

    Update my_string.hpp

--- Final/Task1/my_string.hpp
+++ Final/Task1/my_string.hpp
@@ -28,4 +28,4 @@ public:
 };
 
 #endif // MY_STRING_HPP
-// Reference OPENAI CHATGPT, 2023.  ChatGPT response to James Tulloch, (personal communication, 5th May)
+// Reference OPENAI CHATGPT, 2023.  ChatGPT response to James Tulloch, (personal communication, 1st November)
